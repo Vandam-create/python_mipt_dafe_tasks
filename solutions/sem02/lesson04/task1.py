f47e83bfffa4bf6dd1a33486647b1f74169621b8
Vandam-create <beliakov.va@phystech.edu>
2026-03-13 22:38:02 +0300

    v1

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -1,18 +1,42 @@
+from matplotlib import image
 import numpy as np
 
 
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
-    # ваш код
-    return image
+    H, W = image.shape[:2]
+
+    if(pad_size < 1):
+        raise ValueError
+    
+    if image.ndim == 2:
+        zer = np.zeros((H + 2 * pad_size, W + 2 * pad_size), dtype=image.dtype)
+        zer[pad_size:pad_size + H, pad_size:pad_size + W] = image
+    else:
+        zer = np.zeros((H + 2 * pad_size, W + 2 * pad_size, image.shape[2]), dtype=image.dtype)
+        zer[pad_size:pad_size + H, pad_size:pad_size + W, :] = image
+
+    return zer
 
 
 def blur_image(
     image: np.ndarray,
     kernel_size: int,
 ) -> np.ndarray:
-    # ваш код
-    return image
+    if kernel_size <= 0 or kernel_size % 2 == 0:
+        raise ValueError
 
+    if kernel_size == 1:
+        return image.copy()
+
+    padded_image = pad_image(image, kernel_size // 2)
+    res = np.zeros(image.shape, dtype=float)
+
+    for i in range(kernel_size):
+        for j in range(kernel_size):
+            res += padded_image[i : i + image.shape[0], j : j + image.shape[1]]
+       
+    res /= kernel_size ** 2
+    return res.astype(np.uint8)
 
 if __name__ == "__main__":
     import os
--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -5,6 +5,17 @@ def get_dominant_color_info(
     image: np.ndarray[np.uint8],
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
-    # ваш код
+    if threshold < 1:
+        raise ValueError("threshold must be positive")
 
-    return 0, 0
+    img = image.astype(np.int64)
+    uniq, counts = np.unique(img, return_counts = True)
+    delta = uniq[ :, np.newaxis ] - uniq
+    res = np.abs(delta) < threshold
+    result = np.sum(res * counts, axis = 1)
+    index = np.argmax(result)
+    
+    color = uniq[index]
+    percentage = result[index] / image.size
+    
+    return np.uint8(color), float(percentage)
